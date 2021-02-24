b8fee1061f18b611ba120be6820dd5b758b91ae8
jawabuu <49921621+jawabuu@users.noreply.github.com>
2021-02-24 18:18:44 +0300

    Initial POC

--- terraform/cluster/cluster.tf
+++ terraform/cluster/cluster.tf
@@ -2,86 +2,136 @@ provider "google" {
   project = var.project
 }
 
+# CLUSTER NETWORK
+module "gcp-network" {
+  source       = "terraform-google-modules/network/google"
+  version      = "~> 2.5"
+  project_id   = var.project
+  network_name = var.network
+
+  subnets = [
+    {
+      subnet_name           = var.subnetwork
+      subnet_ip             = "10.0.128.0/17"
+      subnet_region         = var.region
+      subnet_private_access = "true"
+    },
+  ]
+
+  secondary_ranges = {
+    "${var.subnetwork}" = [
+      {
+        range_name    = var.ip_range_pods
+        ip_cidr_range = "192.168.128.0/18"
+      },
+      {
+        range_name    = var.ip_range_services
+        ip_cidr_range = "192.168.192.0/18"
+      },
+    ]
+  }
+}
+
 # CLUSTER
-resource "google_container_cluster" "silvester_cluster" {
-  name = "silvester-cluster"
-  location = var.cluster_location
+module "gke" {
+  source     = "terraform-google-modules/kubernetes-engine/google//modules/private-cluster"
+  version    = "12.3.0"
+  project_id = var.project
+  name       = var.cluster_name
+  regional   = false
+  region     = var.region
+  zones      = [var.cluster_location]
 
-  # We can't create a cluster with no node pool defined, but we want to only use
-  # separately managed node pools. So we create the smallest possible default
-  # node pool and immediately delete it.
-  remove_default_node_pool = true
-  initial_node_count = 1
 
-  workload_identity_config {
-    identity_namespace = "${var.project}.svc.id.goog"
+  network                 = module.gcp-network.network_name
+  subnetwork              = module.gcp-network.subnets_names[0]
+  ip_range_pods           = var.ip_range_pods
+  ip_range_services       = var.ip_range_services
+  create_service_account  = true
+  enable_private_endpoint = false
+  # enable_private_nodes       = true
+  # master_ipv4_cidr_block     = "172.16.0.0/28"
+  http_load_balancing        = false
+  remove_default_node_pool   = true
+  skip_provisioners          = true
+  maintenance_start_time     = "22:00"
+  network_policy             = false
+  monitoring_service         = "none"
+  logging_service            = "none"
+  add_cluster_firewall_rules = var.add_cluster_firewall_rules
+  firewall_inbound_ports     = ["8443", "9443", "15017"]
+  node_pools = [
+    {
+      name         = "ingress-pool"
+      machine_type = "e2-micro"
+      disk_size_gb = 10
+      autoscaling  = false
+      node_count   = 1
+      image_type   = "COS_CONTAINERD"
+      auto_upgrade = true
+      preemptible  = true
+    },
+    {
+      name               = "web-pool"
+      machine_type       = "e2-micro"
+      disk_size_gb       = 10
+      autoscaling        = false
+      initial_node_count = 1
+      node_count         = 1
+      image_type         = "COS_CONTAINERD"
+      auto_upgrade       = true
+      preemptible        = true
+    },
+  ]
+
+  node_pools_oauth_scopes = {
+    all = [
+      "https://www.googleapis.com/auth/cloud-platform",
+      "https://www.googleapis.com/auth/service.management",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/trace.append",
+    ]
+
   }
-}
 
+  node_pools_taints = {
+    all = []
+    ingress-pool = [
 
-# INGRESS NODE POOL
-resource "google_container_node_pool" "silvester_nodepool_ingress" {
-  name     = "silvester-nodepool-ingress"
-  cluster  = google_container_cluster.silvester_cluster.name
-  initial_node_count = 1
-  location = var.cluster_location
+      {
+        key    = "ingress-pool"
+        value  = true
+        effect = "NO_EXECUTE"
+      },
 
-  autoscaling {
-    min_node_count = 1
-    max_node_count = 1
+    ]
   }
 
-  management {
-    auto_repair = true
-    auto_upgrade = true
+  node_pools_tags = {
+    ingress-pool = [
+      "ingress-pool"
+    ]
+    web-pool = [
+      "web-pool"
+    ]
   }
 
-  node_config {
-    workload_metadata_config {
-      node_metadata = "GKE_METADATA_SERVER"
-    }
-    machine_type = "e2-micro" 
-    preemptible  = false 
-    disk_size_gb = 10
-    taint = [ {
-      effect = "NO_SCHEDULE"
-      key = "dedicated"
-      value = "ingress"
-    } ]
-  }
-}
-
-# APPLICATION NODE POOL
-resource "google_container_node_pool" "silvester_nodepool_apps" {
-  name     = "silvester-nodepool-apps"
-  cluster  = google_container_cluster.silvester_cluster.name
-  initial_node_count = 1
-  location = var.cluster_location
-
-  autoscaling {
-    min_node_count = 1
-    max_node_count = 1
-  }
-
-  management {
-    auto_repair = true
-    auto_upgrade = true
-  }
-
-  node_config {
-    workload_metadata_config {
-      node_metadata = "GKE_METADATA_SERVER"
-    }
-    disk_size_gb = 30
-    machine_type = "n2d-highmem-4" 
-    preemptible  = true 
-  }
+  master_authorized_networks = [
+    {
+      display_name = "Anyone"
+      cidr_block   = "0.0.0.0/0"
+    },
+  ]
 }
 
 # HTTP TRAFFIC
 resource "google_compute_firewall" "http_node_port" {
   name    = "http-node-port"
-  network = "default"
+  network = module.gcp-network.network_name
 
   allow {
     protocol = "tcp"
@@ -92,10 +142,27 @@ resource "google_compute_firewall" "http_node_port" {
 # HTTPS TRAFFIC
 resource "google_compute_firewall" "https_node_port" {
   name    = "https-node-port"
-  network = "default"
+  network = module.gcp-network.network_name
 
   allow {
     protocol = "tcp"
     ports    = ["443"]
   }
+}
+
+module "gke_auth" {
+  source       = "terraform-google-modules/kubernetes-engine/google//modules/auth"
+  version      = "12.3.0"
+  project_id   = var.project
+  location     = module.gke.location
+  cluster_name = module.gke.name
+}
+
+module "kubeip" {
+  source                 = "github.com/nufailtd/terraform-budget-gcp//modules/kubeip"
+  project_id             = var.project
+  zone                   = module.gke.location
+  host                   = module.gke_auth.host
+  cluster_ca_certificate = module.gke_auth.cluster_ca_certificate
+  token                  = module.gke_auth.token
 }
--- terraform/cluster/variables.tf
+++ terraform/cluster/variables.tf
@@ -1,9 +1,43 @@
 variable "project" {
-    type = string
-    default = "silvester-304916"
+  type    = string
+  default = "silvester-304916"
+}
+
+variable "region" {
+  type    = string
+  default = "europe-west1"
 }
 
 variable "cluster_location" {
-    type = string
-    default = "europe-west1-b"
+  type    = string
+  default = "europe-west1-b"
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "silvester-cluster"
+}
+
+variable "add_cluster_firewall_rules" {
+  default = false
+}
+
+variable "network" {
+  type    = string
+  default = "default"
+}
+
+variable "subnetwork" {
+  type    = string
+  default = "gke-subnet"
+}
+
+variable "ip_range_pods" {
+  type    = string
+  default = "ip-range-pods"
+}
+
+variable "ip_range_services" {
+  type    = string
+  default = "ip-range-scv"
 }
